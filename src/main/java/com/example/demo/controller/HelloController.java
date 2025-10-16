8f4e358a47a6cc2a73aa5e90562740a952c5b293
nds13755 <nds1375501@126.com>
2025-10-16 18:02:06 +0800

    修改  HelloController  显示内容

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/hello1")
     public String hello() {
         return "Hello Springboot!";
     }
